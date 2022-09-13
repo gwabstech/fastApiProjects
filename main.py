6905abdc4ce83524c6ceff7e5534b1249f0b5ce1
Abubakar <Gwabstech@gmail.com>
2022-09-13 12:48:00 +0100

    understanding path and query params

--- main.py
+++ main.py
@@ -5,31 +5,40 @@ from fastapi import FastAPI
 app = FastAPI()
 
 #path and queryparams
+
+
 @app.get("/blog/all")
-def get_all_blogs(page:int = 1,pageSize:Optional[int] = 1):
+def get_all_blogs(page: int = 1, pageSize: Optional[int] = 1):
     return {"message": f"All blogs retuned for page {page}  size {pageSize}"}
 
+
+@app.get("/")
+def welcome():
+    return {"message": 'Welcome to the new api'}
+
+
+# path parameters
+@app.get("/spesific_blog/{id}")
+def get_blog(id: int):
+    return {"message": f"blog number {id}"}
+
+
 @app.get("/blog/{page}")
-def gt1(page:int,pageSize:int = 1):
+def gt1(page: int, pageSize: int = 1):
     return {"message": f"the page is {page} and the size is {pageSize}"}
 
-class BlogType(str,Enum):
+
+class BlogType(str, Enum):
     short = "short"
     story = "story"
     howTo = "howTo"
 
 
 @app.get("/blog/type/{type}")
-def getBlogType(type:BlogType):
-    return {"messge":f"Blog type {type}"}
+def getBlogType(type: BlogType):
+    return {"messge": f"Blog type {type}"}
 
-@app.get("/blog/{id}")
-def get_glog(id: int):
-    return {"message": f"blog number {id}"}
 
 @app.get("/blog/{id}/comments/{comment_id}")
-def getComment(id:int,comment_id:int,valid:bool = True,username:Optional[str] = None):
+def getComment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid},username {username}'}
-
-
-
